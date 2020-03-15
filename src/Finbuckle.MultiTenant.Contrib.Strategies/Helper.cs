b0739e638587435ee5c75c921d4bebccc31dd978
Chad Bengen <chad@visitingangelswa.com>
2020-03-15 12:22:37 -0700

    add cookie strategy

---
+++ src/Finbuckle.MultiTenant.Contrib.Strategies/Helper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Finbuckle.MultiTenant.Contrib.Strategies
+{
+    public static class Helper
+    {
+        public static void SetTenantCookie(HttpResponse response, string tenantKey, string tenantId)
+        {            
+            if (string.IsNullOrWhiteSpace(tenantId))
+            {
+                response.Cookies.Delete(tenantKey);
+            }
+            else
+            {
+                var encrypted = AesOperation.EncryptString(AesOperation.Key, tenantId);
+                //TODO: encrypt value
+                response.Cookies.Append(
+                    tenantKey,
+                    encrypted,
+                    new CookieOptions
+                    {
+                        Path = "/",
+                        HttpOnly = false,
+                        Expires = DateTimeOffset.Now.AddYears(10)
+                    }
+                );
+            }
+        }
+    }
+}
